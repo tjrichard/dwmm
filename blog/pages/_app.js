032cf2bf7b44a14babdd14da1ba286e4d19cd333
Ryan-Jang <tjrichard91@gmail.com>
2024-08-14 16:24:58 +0900

    Revert "vercel installed"
    This reverts commit 00f17b0495eaec359ab1e394920bdac6070bf0f4.

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,11 +1,9 @@
 import React from "react";
 import "../styles/main.scss";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
       <Component {...pageProps} />
-      <SpeedInsights />
   );
 }
 
