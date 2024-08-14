00f17b0495eaec359ab1e394920bdac6070bf0f4
Ryan-Jang <tjrichard91@gmail.com>
2024-08-14 11:58:56 +0900

    vercel installed

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../styles/main.scss";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
       <Component {...pageProps} />
+      <SpeedInsights />
   );
 }
 
