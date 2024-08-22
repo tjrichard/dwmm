409c336b63e9c618804dc9644442181116d9a878
Seunghwan <tjrichard91@gmail.com>
2024-08-22 14:10:26 +0900

    meta 정보 덮어쓰기 방지

--- blog/components/header.js
+++ blog/components/header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "../public/logo.svg";
 import Link from "next/link";
 
 function Header() {
--- blog/components/meta.js
+++ blog/components/meta.js
@@ -5,7 +5,11 @@ const Meta = ({ title, description }) => (
   <Head>
     <title>{title}</title>
     <meta name="description" content={description} />
+    <meta name="description" content="DWMM" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content="/static/default-image.png" />
     {/* 추가적인 메타 태그나 링크 삽입 가능 */}
   </Head>
 );
