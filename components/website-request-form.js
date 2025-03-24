d115ece3d9c9f42ac34fe14633b83043dcb7e526
Ryan-Jang <tjrichard91@gmail.com>
2025-03-24 20:14:31 +0900

    Update website-request-form.js

--- components/website-request-form.js
+++ components/website-request-form.js
@@ -22,7 +22,7 @@ const genAI = new GoogleGenerativeAI(geminiApiKey)
 
 const model = genAI.getGenerativeModel({
   model: 'gemini-2.0-flash',
-  systemInstruction: '제공되는 링크를 방문해 제공된 structured output을 결과로 전달해줘. title 은 가능한 해당 웹사이트의 meta title 혹은 <h> 태그를 그대로 사용하되, 설명식으로 적혀져있다면 실제 이름을 유추해서 명사형으로 작성해줘. description은 LLM 을 통해 사이트의 역할, 내용, 콘텐츠를 분석해 검색에 사용될만한 meta keyword들을 리스트업 해줘. 이 때 한국어와 영어로 모두 작성해주고 발생할 수 있는 오타를 염두에 두고 가능한 많이 작성해줘. 각 항목은 \',\' 로 분리하면 돼. category는 너가 주어진 enum 타입 중에서 가장 적절한 카테고리를 1개 정해줘. 특정 주제의 콘텐츠를 모아주는 역할을 하는 사이트의 경우에는 \'Collection\' 을 사용해. tags 는 너가 사이트의 역할, 내용, 콘텐츠를 분석해 적절한 태그를 3개에서 5개 사이로 만들어줘. tags 는 Branding, Company, curation, Inspiration, Mockup, OfflineDesign, Pattern, PitchDeck, Portfolio, portfolio, SaaS, Store, UXPattern 이와 같이 영문으로 1개에서 2개 단어로 구성되어야 하고 사이트의 제목을 포함해서는 안돼.',
+  systemInstruction: '제공되는 링크를 방문해 제공된 structured output을 결과로 전달해줘. 결과를 생성하는데 할루시네이션이 절대 발생하면 안돼. 정확하지 않다면 "?"를 제공해줘. title 은 가능한 해당 웹사이트의 meta title 혹은 <h> 태그를 그대로 사용하되, 설명식으로 적혀져있다면 실제 이름을 유추해서 명사형으로 작성해줘. description은 LLM 을 통해 사이트의 meta name="title", meta name="description", meta name="keywords", meta property="og:description", 사이트 콘텐츠를 분석해 검색에 사용될만한 키워드들을 리스트업 해줘. 이 때 한국어와 영어로 모두 작성해주고 발생할 수 있는 오타를 염두에 두고 가능한 많이 작성해줘. 각 항목은 \',\' 로 분리하면 돼. category는 너가 주어진 enum 타입 중에서 가장 적절한 카테고리를 1개 정해줘. 특정 주제의 콘텐츠를 모아주는 역할을 하는 사이트의 경우에는 \'Collection\' 을 사용해. tags 는 너가 사이트의 역할, 내용, 콘텐츠를 분석해 적절한 태그를 3개에서 5개 사이로 만들어줘. tags 는 Branding, Company, curation, Inspiration, Mockup, OfflineDesign, Pattern, PitchDeck, Portfolio, portfolio, SaaS, Store, UXPattern 이와 같이 영문으로 1개에서 2개 단어로 구성되어야 하고 사이트의 제목을 포함해서는 안돼.',
 })
 
 const generationConfig = {
