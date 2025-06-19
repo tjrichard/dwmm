800ec8215e7910e59f7c844ee851d5c6f906233b
Ryan-Jang <tjrichard91@gmail.com>
2025-06-19 18:59:16 +0900

    ContentGrid 컴포넌트에서 마지막 항목에 대한 key 속성을 추가하여 이미지가 실제 bookmark id의 img 를 반영하지 못하는 문제를 해결했습니다.

--- components/ContentGrid.js
+++ components/ContentGrid.js
@@ -7,7 +7,7 @@ function ContentGrid({ contents = [], isSearching = false, lastBookmarkRef = nul
       {contents.map((content, index) => {
         const isLastItem = contents.length === index + 1
         return isLastItem ? (
-          <div ref={lastBookmarkRef} className="fade-in">
+          <div key={content.id} ref={lastBookmarkRef} className="fade-in">
             <ContentCard 
               content={content} 
               onCategoryClick={onCategoryClick}
@@ -15,7 +15,7 @@ function ContentGrid({ contents = [], isSearching = false, lastBookmarkRef = nul
             />
           </div>
         ) : (
-          <div className="fade-in">
+          <div key={content.id} className="fade-in">
             <ContentCard 
               content={content} 
               onCategoryClick={onCategoryClick}
